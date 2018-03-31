bef36b1118c4a4f0261759923c38fecfa18c20cf
Birat Rai <biratrai@hotmail.com>
2018-03-31 09:03:57 -0400

    adding ui for signin

---
+++ web-react/src/components/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {FontIcon, RaisedButton} from "material-ui";
+import {loginWithGoogle} from "../helpers/auth";
+import {firebaseAuth} from "../constants/firebaseconst";
+
+const SignIn = () =>
+  <div>
+    <h1>SignIn Page</h1>
+  </div>
+
+export default SignIn;
