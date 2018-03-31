e6ad98644930bf7db38b7613b475d9f8ad5b3e92
Birat Rai <biratrai@hotmail.com>
2018-03-31 11:01:44 -0400

    adding home feature with sign out material button

--- web-react/src/components/Home.js
+++ web-react/src/components/Home.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import {Avatar, RaisedButton} from "material-ui";
+import {logout} from "../helpers/auth";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const Home = () =>
-  <div>
-    <h1>Home Page</h1>
-  </div>
+const appTokenKey = "appToken"; // also duplicated in Login.js
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
 
-export default Home;
+        this.state = {
+            //firebaseUser: JSON.parse(localStorage.getItem("firebaseUser"))
+        };
+
+        //console.log("User:", this.state.firebaseUser);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogout() {
+        logout().then(function () {
+            localStorage.removeItem(appTokenKey);
+            this.props.history.push("/login");
+            console.log("user signed out from firebase");
+        }.bind(this));
+
+    }
+
+	render() {
+    	return (
+			<div>
+                <h1>List of project submitted</h1>
+ 				<MuiThemeProvider>
+                <div>
+                    <RaisedButton
+                        backgroundColor="#a4c639"
+                        labelColor="#ffffff"
+                        label="Sign Out"
+                        onTouchTap={this.handleLogout}
+                    />
+                </div>
+                </MuiThemeProvider>
+            </div>
+        );
+    }
+}
