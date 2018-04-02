8f9f89a18aed8c3aabadd6e965e33aab83b5309a
Birat Rai <biratrai@hotmail.com>
2018-04-02 14:41:01 -0400

    fetching data from server and adding multiple projects

--- web-react/src/components/Home.js
+++ web-react/src/components/Home.js
@@ -20,16 +20,85 @@ const RenderLogOut = props =>  (
       </MuiThemeProvider>
 );
 
+const RenderAddProject = props => (
+  <MuiThemeProvider>
+    <div>
+        <RaisedButton
+            backgroundColor="#a4c639"
+            labelColor="#ffffff"
+            label="Add Project"
+            onTouchTap={this.addProject}
+        />
+    </div>
+  </MuiThemeProvider>
+)
+
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
             //firebaseUser: JSON.parse(localStorage.getItem("firebaseUser"))
+            projects : []
         };
 
         //console.log("User:", this.state.firebaseUser);
         this.handleLogout = this.handleLogout.bind(this);
+        this.nextId = this.nextId.bind(this)
+        this.update = this.update.bind(this)
+        this.eachProject = this.eachProject.bind(this)
+    }
+
+    componentWillMount() {
+
+      var self = this
+      console.log('Component Will Mount')
+      console.log(this.props.count)
+		    if(this.props.count) {
+			       fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
+				         .then(response => response.json())
+				             .then(json => json[0]
+								               .split('. ')
+								               .forEach(sentence => self.addProject(sentence.substring(0, 25))))
+		    }
+    }
+
+    addProject(text) {
+        this.setState(prevState => ({
+          projects: [
+            ...prevState.projects,
+            {
+              id: this.nextId(),
+              url: text,
+              image: 'project.png'
+            }
+          ]
+        }))
+    }
+
+    nextId() {
+    		this.uniqueId = this.uniqueId || 0
+    		return this.uniqueId++
+    }
+
+    eachProject(project, i) {
+    		return (
+    			<Project key={i}
+    				  index={i}
+    				  onChange={this.update}
+    				  onRemove={this.remove}>
+    				  {project.url}
+    		    </Project>
+    		)
+    }
+
+    update(newText, i) {
+    		console.log('updating item at index', i, newText)
+    		this.setState(prevState => ({
+    			projects: prevState.projects.map(
+    				project => (project.id !== i) ? project : {...project, project: newText}
+    			)
+    		}))
     }
 
     handleLogout() {
@@ -45,7 +114,10 @@ export default class Home extends React.Component {
 			     <div>
               <h1>List of project submitted</h1>
               <RenderLogOut/>
-              <Project/>
+              {this.state.projects.map(this.eachProject)}
+              <div >
+                <RenderAddProject/>
+              </div>
            </div>
       );
     }
