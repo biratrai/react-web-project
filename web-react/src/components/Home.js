e0ead4b701038a34e099672efb9b6b1854b04f5c
Birat Rai <biratrai@hotmail.com>
2018-04-02 12:42:16 -0400

    extracting components for signOut and renderProject

--- web-react/src/components/Home.js
+++ web-react/src/components/Home.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import {Avatar, RaisedButton} from "material-ui";
 import {logout} from "../helpers/auth";
+import logo from './logo.svg';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const appTokenKey = "appToken"; // also duplicated in Login.js
+
+const RenderLogOut = props =>  (
+
+      <MuiThemeProvider>
+        <div>
+            <RaisedButton
+                backgroundColor="#a4c639"
+                labelColor="#ffffff"
+                label="Sign Out"
+                onTouchTap={this.handleLogout}
+            />
+        </div>
+      </MuiThemeProvider>
+);
+
+const RenderProject = props => (
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <a href="">Link to project</a>
+        </div>
+);
+
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
@@ -14,6 +37,8 @@ export default class Home extends React.Component {
 
         //console.log("User:", this.state.firebaseUser);
         this.handleLogout = this.handleLogout.bind(this);
+        // this.renderLogOut = this.renderLogOut.bind(this);
+        // this.renderProject = this.renderProject.bind(this);
     }
 
     handleLogout() {
@@ -21,25 +46,18 @@ export default class Home extends React.Component {
             localStorage.removeItem(appTokenKey);
             this.props.history.push("/login");
             console.log("user signed out from firebase");
-        }.bind(this));
-
+        });
     }
 
 	render() {
     	return (
-			<div>
-                <h1>List of project submitted</h1>
- 				<MuiThemeProvider>
-                <div>
-                    <RaisedButton
-                        backgroundColor="#a4c639"
-                        labelColor="#ffffff"
-                        label="Sign Out"
-                        onTouchTap={this.handleLogout}
-                    />
-                </div>
-                </MuiThemeProvider>
-            </div>
-        );
+			     <div>
+              <h1>List of project submitted</h1>
+              <RenderLogOut/>
+              <RenderProject/>
+           </div>
+
+
+      );
     }
 }
