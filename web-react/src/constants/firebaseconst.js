046de6dcc32503ac6317f98b095cc4f977e4e923
Birat Rai <biratrai@hotmail.com>
2018-04-03 19:37:30 -0400

    improving the const for firebase

--- web-react/src/constants/firebaseconst.js
+++ web-react/src/constants/firebaseconst.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "your-firebase-api",
-    authDomain: "your-firebase-project.firebaseapp.com",   
-	databaseURL: "https://your-firebase-project.firebaseio.com",
+  apiKey: "your-firebase-api",
+  authDomain: "your-firebase-project.firebaseapp.com",
+  databaseURL: "https://your-firebase-project.firebaseio.com",
 };
 
 firebase.initializeApp(config);
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
-export const ref = firebase.database().ref();
+export const database = firebase.database();
 export const firebaseAuth = firebase.auth;
