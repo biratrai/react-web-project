d017a6b668f7522e97d65a749117d1250eb9911d
Birat Rai <biratrai@hotmail.com>
2018-04-03 21:58:09 -0400

    removing unnecessary routes

--- web-react/src/components/AppNavigation.js
+++ web-react/src/components/AppNavigation.js
@@ -5,12 +5,9 @@ import {
 } from 'react-router-dom';
 
 import Navigation from './Navigation';
-import LandingPage from './Landing';
-import SignUpPage from './SignUp';
 import SignInPage from './SignIn';
-import PasswordForgetPage from './PasswordForget';
+import AddProject from './AddProject';
 import HomePage from './Home';
-import AccountPage from './Account';
 
 import * as routes from '../constants/routes';
 
@@ -18,33 +15,17 @@ const AppNavigation = () =>
   <Router>
     <div>
       <Navigation />
-
       <hr/>
+      <Route
+        exact path={routes.ADD_PROJECT}
+        component={() => <AddProject />}
+      />
 
-      <Route
-        exact path={routes.LANDING}
-        component={() => <LandingPage />}
-      />
-      <Route
-        exact path={routes.SIGN_UP}
-        component={() => <SignUpPage />}
-      />
-      <Route
-        exact path={routes.SIGN_IN}
-        component={() => <SignInPage />}
-      />
-      <Route
-        exact path={routes.PASSWORD_FORGET}
-        component={() => <PasswordForgetPage />}
-      />
       <Route
         exact path={routes.HOME}
         component={() => <HomePage count = {50} />}
       />
-      <Route
-        exact path={routes.ACCOUNT}
-        component={() => <AccountPage />}
-      />
+
     </div>
   </Router>
 
