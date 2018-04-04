1f51d6204283e3458a476f734b08918cc7dc86c3
Birat Rai <biratrai@hotmail.com>
2018-04-03 21:57:59 -0400

    adding component to add project

---
+++ web-react/src/components/AddProject.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {database} from "../constants/firebaseconst";
+
+const SignOut = () =>
+  <div>
+    <h1>Please add project</h1>
+
+      <form>
+        <textarea placeholder="Enter your pencode url here."/>
+        <button>Submit</button>
+      </form>
+
+  </div>
+
+export default SignOut;
