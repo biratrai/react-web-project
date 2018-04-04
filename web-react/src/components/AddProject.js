2186f9e6e139c145f9156dd61f607d855a2de13a
Birat Rai <biratrai@hotmail.com>
2018-04-03 22:38:47 -0400

    adding form input to submit url and screenshot

--- web-react/src/components/AddProject.js
+++ web-react/src/components/AddProject.js
@@ -1,29 +1,75 @@
 import React from 'react';
 import {database} from "../constants/firebaseconst";
 
-const RenderForm = () => (
-  <div>
-    <h1>Please add project</h1>
-
-      <form>
-        <textarea placeholder="Enter your pencode url here."/>
-        <button>Submit</button>
-      </form>
-
-  </div>
-)
+// const RenderForm = ({addToFirebase}) => (
+//   <div>
+//     <h1>Please add project</h1>
+//
+//       <form>
+//         <label> Pencode url.</label>
+//         <input type="text" name="url" placeholder="Enter url" value={props.state.url} onChange={this.handleUrlChange}/>
+//         <br/>
+//         <br/>
+//         <label> Screen shot.</label>
+//         <input type="text" name="image" placeholder="Upload screenshot" value={props.state.image} onChange={this.handleImageChange}/>
+//         <br/>
+//         <br/>
+//         <button onClick={addToFirebase}>Submit</button>
+//       </form>
+//
+//   </div>
+// )
 
 export default  class AddProject extends React.Component {
+  constructor(){
+      super();
+      this.state = {
+         url : '',
+         image : ''
+      };
+
+      this.handleUrlChange = this.handleUrlChange.bind(this);
+      this.handleImageChange = this.handleImageChange.bind(this);
+      this.addToFirebase = this.addToFirebase.bind(this);
+  }
+
   componentWillMount() {
     console.log('Add Project Component Will Mount')
   }
 
+  handleUrlChange (evt) {
+    this.setState({ url: evt.target.value });
+    console.log(this.state.url);
+  }
+
+  handleImageChange (evt) {
+    this.setState({ image: evt.target.value });
+  }
+
+  addToFirebase (evt){
+    console.log("add to firebase");
+    console.log("Url: "+this.state.url);
+    console.log("screenshot: "+this.state.screenshot);
+  }
 
   render(){
     return (
-        <div>
-          <RenderForm/>
-        </div>
+      <div>
+        <h1>Please add project</h1>
+
+          <form>
+            <label> Pencode url: </label>
+            <input type="text" name="url" placeholder="Enter url" value={this.state.url} onChange={this.handleUrlChange}/>
+            <br/>
+            <br/>
+            <label> Screen shot: </label>
+            <input type="text" name="image" placeholder="Upload screenshot" value={this.state.image} onChange={this.handleImageChange}/>
+            <br/>
+            <br/>
+            <button onClick={this.addToFirebase}>Submit</button>
+          </form>
+
+      </div>
     )
   }
 }
