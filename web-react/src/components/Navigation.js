11b34e21e73dbc49e6bfd1b0f6fe5d45c011f870
Birat Rai <biratrai@hotmail.com>
2018-04-03 21:58:22 -0400

    adding link to add project

--- web-react/src/components/Navigation.js
+++ web-react/src/components/Navigation.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Avatar, RaisedButton} from "material-ui";
 import * as routes from '../constants/routes';
 
 const Navigation = () =>
   <div>
     <ul>
-      <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-      <li><Link to={routes.LANDING}>Landing</Link></li>
+
+      <li><Link to={routes.ADD_PROJECT}>Add Project</Link></li>
+
       <li><Link to={routes.HOME}>Home</Link></li>
-      <li><Link to={routes.ACCOUNT}>Account</Link></li>
     </ul>
   </div>
 
