e84fae9772a4a4d51dbe08174aab0aa01d6d17f8
Birat Rai <biratrai@hotmail.com>
2018-04-05 21:43:37 -0400

    adding logic to pass file to firebase

--- web-react/src/components/AddProject.js
+++ web-react/src/components/AddProject.js
@@ -25,7 +25,7 @@ export default  class AddProject extends React.Component {
       super();
       this.state = {
          url : '',
-         image : null
+         image : ''
       };
 
       this.handleUrlChange = this.handleUrlChange.bind(this);
@@ -38,19 +38,32 @@ export default  class AddProject extends React.Component {
   }
 
   handleUrlChange (evt) {
+    evt.preventDefault();
     this.setState({ url: evt.target.value });
     console.log(this.state.url);
   }
 
   handleImageChange (evt) {
-    this.setState({ image: evt.target.files[0] });
-    console.log("image: "+ evt.target.files[0]);
+    evt.preventDefault();
+    // this.setState({ image: evt.target.files[0] });
+    let reader = new FileReader();
+    let file = evt.target.files[0];
+    console.log('file '+ file);
+    reader.onloadend = () => {
+      this.setState({
+        image: file
+      });
+    }
+    // reader.readAsDataURL(file);
+    console.log("image: "+ this.state.image);
   }
 
   addToFirebase (evt){
     evt.preventDefault();
     console.log("add to firebase");
     console.log("Url: "+this.state.url);
+    console.log("url: "+ this.urlLink.value)
+    console.log('fils: '+ this.uploadInput.files[0])
     console.log("screenshot: "+this.state.screenshot);
   }
 
@@ -61,11 +74,12 @@ export default  class AddProject extends React.Component {
 
           <form>
             <label> Pencode url: </label>
-            <input type="text" name="url" placeholder="Enter url" value={this.state.url} onChange={this.handleUrlChange}/>
+            <input type="text" name="url" placeholder="Enter url" ref={(ref) => { this.urlLink = ref; }}/>
             <br/>
             <br/>
             <label> Screen shot: </label>
-            <input type="file" name="image" placeholder="Upload screenshot" value={this.state.image} onChange={this.handleImageChange}/>
+            <input ref={(ref) => { this.uploadInput = ref; }} placeholder="Upload screenshot" type="file" />
+
             <br/>
             <br/>
             <button onClick={this.addToFirebase}>Submit</button>
