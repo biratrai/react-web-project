a3196a4e704ea597b547a83f8354bd90314e1569
Birat Rai <biratrai@hotmail.com>
2018-04-08 12:16:52 -0400

    removing unused code

--- web-react/src/components/AddProject.js
+++ web-react/src/components/AddProject.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import {database} from "../constants/firebaseconst";
-
-// const RenderForm = ({addToFirebase}) => (
-//   <div>
-//     <h1>Please add project</h1>
-//
-//       <form>
-//         <label> Pencode url.</label>
-//         <input type="text" name="url" placeholder="Enter url" value={props.state.url} onChange={this.handleUrlChange}/>
-//         <br/>
-//         <br/>
-//         <label> Screen shot.</label>
-//         <input type="text" name="image" placeholder="Upload screenshot" value={props.state.image} onChange={this.handleImageChange}/>
-//         <br/>
-//         <br/>
-//         <button onClick={addToFirebase}>Submit</button>
-//       </form>
-//
-//   </div>
-// )
+import {firebaseAuth} from "../constants/firebaseconst";
+import firebase from 'firebase';
+import Login from './SignIn'
 
 export default  class AddProject extends React.Component {
   constructor(){
@@ -31,10 +15,11 @@ export default  class AddProject extends React.Component {
       this.handleUrlChange = this.handleUrlChange.bind(this);
       this.handleImageChange = this.handleImageChange.bind(this);
       this.addToFirebase = this.addToFirebase.bind(this);
+      this.loginCallBack = this.loginCallBack.bind(this);
   }
 
   componentWillMount() {
-    console.log('Add Project Component Will Mount')
+    console.log('Add Project Component Will Mount'+ firebaseAuth.currentUser)
   }
 
   handleUrlChange (evt) {
@@ -51,53 +36,64 @@ export default  class AddProject extends React.Component {
     console.log('file '+ file);
     reader.onloadend = () => {
       this.setState({
-        image: file
+        profilePic: '',
+        userName: ''
       });
     }
     // reader.readAsDataURL(file);
     console.log("image: "+ this.state.image);
   }
 
+  loginCallBack = (dataFromChild) => {
+      console.log('loginCallBack dataFromChild : '+ dataFromChild)
+      if(dataFromChild != null){
+        this.setState({
+          profilePic: dataFromChild.photoURL,
+          userName: dataFromChild.displayName
+        })
+      }
+  }
+
   addToFirebase (evt){
     evt.preventDefault();
     console.log("add to firebase");
-    console.log("Url: "+this.state.url);
+    console.log("profilePic: "+this.state.profilePic);
     console.log("url: "+ this.urlLink.value)
-    console.log('fils: '+ this.uploadInput.files[0])
-    console.log("screenshot: "+this.state.screenshot);
+    console.log("userName: "+this.state.userName);
 
+    if(typeof this.state.userName === 'undefined'){
+      alert('Not signed in. Please log in to submit.')
+      return;
+    }
     const databaseRef = database.ref('projects');
 
     const project = {
       id: 'id',
-      url: 'https://codepen.io/LeafDragon/full/vdvXdO/',
+      url: this.urlLink.value,
       image: 'image',
-      profilePic : 'profilePic',
-      userName : 'userName'
+      profilePic : this.state.profilePic,
+      userName : this.state.userName
     }
 
-    databaseRef.push(project);
+    // databaseRef.push(project);
 
   }
 
   render(){
     return (
       <div>
-        <h1>Please add project</h1>
+          <Login  callbackFromParent={this.loginCallBack}/>
+        <div>
+          <h1>Please add project</h1>
 
-          <form>
-            <label> Pencode url: </label>
-            <input type="text" name="url" placeholder="Enter url" ref={(ref) => { this.urlLink = ref; }}/>
-            <br/>
-            <br/>
-            <label> Screen shot: </label>
-            <input ref={(ref) => { this.uploadInput = ref; }} placeholder="Upload screenshot" type="file" />
+            <form>
+              <label> Pencode url: </label>
+              <input type="text" name="url" placeholder="Enter url" ref={(ref) => { this.urlLink = ref; }}/>
+              <br/>
 
-            <br/>
-            <br/>
-            <button onClick={this.addToFirebase}>Submit</button>
-          </form>
-
+              <button onClick={this.addToFirebase}>Submit</button>
+            </form>
+          </div>
       </div>
     )
   }
