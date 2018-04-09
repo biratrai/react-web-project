23c3a997ff40766427c729117abe195563b31d5c
Birat Rai <biratrai@hotmail.com>
2018-04-09 19:52:49 -0400

    adding the deleted code

--- web-react/src/components/AppNavigation.js
+++ web-react/src/components/AppNavigation.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
+  Route,
 } from 'react-router-dom';
 
 import Navigation from './Navigation';
+import AddProject from './AddProject';
+import HomePage from './Home';
+
+import * as routes from '../constants/routes';
 
 const AppNavigation = () =>
   <Router>
     <div>
-      <Navigation />
+    <Navigation />
+      <hr/>
+      <Route
+        exact path={routes.ADD_PROJECT}
+        component={() => <AddProject />}
+      />
+
+      <Route
+        exact path={routes.HOME}
+        component={() => <HomePage count = {50} />}
+      />
+
     </div>
   </Router>
 
