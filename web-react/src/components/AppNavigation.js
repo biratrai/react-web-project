37316a74d11c451b5e470fd7b4eff72fc9e87635
Birat Rai <biratrai@hotmail.com>
2018-04-09 19:40:46 -0400

    cleaning unused imports

--- web-react/src/components/AppNavigation.js
+++ web-react/src/components/AppNavigation.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route,
 } from 'react-router-dom';
 
 import Navigation from './Navigation';
-import AddProject from './AddProject';
-import HomePage from './Home';
-
-import * as routes from '../constants/routes';
 
 const AppNavigation = () =>
   <Router>
     <div>
-      <Navigation />    
+      <Navigation />
     </div>
   </Router>
 
